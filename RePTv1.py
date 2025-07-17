a9f3481d7c57982ed6f4e0916f72dc1c13952369
Steven-Sturrock <steven.k.sturrock@gmail.com>
2025-07-18 10:16:13 +1200

    V0.0.2
    Created constants for column names and current date variables for validation

--- RePTv1.py
+++ RePTv1.py
@@ -3,8 +3,20 @@
 
 '''
 V0.0.1 - Initial version 
+V0.0.2 - Created constants for column names and current date variables for validation
 '''
 
+#Libraries
 from tkinter import *
 import csv
 import datetime
+
+#Column names
+ROUTE = 'Route'
+VEHICLE = 'Vehicle'
+DATE = 'Date'
+
+#Current date (validation)
+D = datetime.datetime.now().strftime("%d")
+M = datetime.datetime.now().strftime("%m")
+Y = datetime.datetime.now().strftime("%Y")
